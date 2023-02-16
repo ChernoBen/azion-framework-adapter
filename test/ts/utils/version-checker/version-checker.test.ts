c7d3b4cc775e27e5269aeb6615f3158b5b0d331e
benjamim.neto <benjamim.neto@azion.com>
2023-02-16 16:52:35 -0300

    test: adding nextjs checker method

--- test/ts/utils/version-checker/version-checker.test.ts
+++ test/ts/utils/version-checker/version-checker.test.ts
@@ -1,11 +1,14 @@
 import { VersionChecker } from "../../../../dist/utils/version-checker/version-checker";
 import { expect } from "chai";
 
-describe("Version Checker test suite",()=>{
+describe.only("Version Checker test suite",()=>{
 
     it("should verify if node_version method was called with success",()=>{
         expect(VersionChecker.node_version()).equal(true);
     });
 
+    it("should verify if nextjs_version was called with success",()=>{
+        expect(VersionChecker.nextjs_version(`${process.cwd()}/test/mocks`)).equal(true);
+    });
 });
 
